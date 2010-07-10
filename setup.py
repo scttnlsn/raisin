f0b33d9c99ade5344439aa63f3159be25b29295c
Scott Nelson <scottbnel@gmail.com>
2010-07-10 10:17:34 -0400

    Added python-mysql install dependency

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(
     author_email = 'scottbnel@gmail.com',
     url = 'http://www.github.com/scottbnel/raisin/',
     packages = find_packages(),
+    install_requires = ['mysql-python>=1.2.2'],
     zip_safe = True,
 )
